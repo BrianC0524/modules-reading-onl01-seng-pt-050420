6ff0d6fbcca81a388736133f741eb25272378464
Brian Craycraft <craycraftb24@hotmail.com>
2020-06-18 19:48:14 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,9 +3,9 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid
   include Dance
-  include FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   extend MetaDancing
-  extend FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
   
   attr_accessor :name
   
